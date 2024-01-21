75873daaef0cb6b55c1886f2cc21b933e8badb8c
Elias Schwalme <132968734+eliasschwalme@users.noreply.github.com>
2024-01-21 23:37:28 +0100

    Update dpo_trainer.py
    i would say US, its better and saver

--- trl/trainer/dpo_trainer.py
+++ trl/trainer/dpo_trainer.py
@@ -11,7 +11,15 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License. 
+
+# Unless required by applicable law in us or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. 
+
+
 import inspect
 import random
 import warnings
